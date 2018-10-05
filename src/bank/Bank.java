fec782887a5cfbd4ecc8624fa52a978b1609043a
Do <Do@joon>
2018-10-05 21:04:30 +0900

    Update : add Exception handling

--- src/bank/Bank.java
+++ src/bank/Bank.java
@@ -5,20 +5,24 @@ public class Bank {
 		
 		Functions fc = new Functions();
 		while(true) {
-			int selectMenu = fc.menuPrint();
-			if(selectMenu == 1) {
-				fc.createAccount();
-			} else if (selectMenu == 2) {
-				fc.accountList();
-			} else if (selectMenu ==3) {
-				fc.deposit();
-			} else if(selectMenu ==4) {
-				fc.withdraw();
-			} else if(selectMenu == 5) {
-				System.out.println("시스템을 종료합니다.");
-				break;
-			} else {
-				System.out.println("메뉴를 다시 선택하세요.");
+			try {
+				int selectMenu = fc.menuPrint();
+				if(selectMenu == 1) {
+					fc.createAccount();
+				} else if (selectMenu == 2) {
+					fc.accountList();
+				} else if (selectMenu ==3) {
+					fc.deposit();
+				} else if(selectMenu ==4) {
+					fc.withdraw();
+				} else if(selectMenu == 5) {
+					System.out.println("시스템을 종료합니다.");
+					break;
+				} else {
+					System.out.println("메뉴를 다시 선택하세요.");
+				}
+			}catch (Exception e) {
+				System.out.println("###숫자를 입력하세요###");
 			}
 		}
 	}
