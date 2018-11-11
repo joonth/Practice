65bc2bfa71774b9a963b592c6d0611dcc8264f5b
Joon <joonthy@gmail.com>
2018-11-11 18:04:58 +0900

    Fix logout controller

--- Servlet_CRUD/WebContent/Header.jsp
+++ Servlet_CRUD/WebContent/Header.jsp
@@ -4,5 +4,5 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <div style="background-color:#00008b; color:#ffffff; height:20px; padding:5px;">
-	PRACTICE [${smember.getMname()}] <a href='Logout'>[로그아웃]</a>
+	PRACTICE [${smember.getMname()}] <a href='Logout.do'>[로그아웃]</a>
 </div>
--- Servlet_CRUD/src/member/ServletLogout.java
+++ Servlet_CRUD/src/member/ServletLogout.java
@@ -1,6 +1,8 @@
 package member;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,13 +10,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.MemberDao;
+import vo.Member;
+
 
 @WebServlet("/Logout")
 public class ServletLogout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getSession().invalidate();
-		response.sendRedirect("Login");
+		HttpSession session = request.getSession();
+		Member smember =(Member)session.getAttribute("smember");
+		if(smember != null) {
+			request.getSession().invalidate();
+			request.setAttribute("viewUrl", "redirect:form/LoginForm.jsp");
+		}else {
+			request.setAttribute("viewUrl", "redirect:form/LoginForm.jsp");
+		}
 	}
 }
